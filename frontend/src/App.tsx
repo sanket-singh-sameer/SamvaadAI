4ee109f181d4552f97317d5734ef800fa4743ff2
divyamsingh007 <legendprice007@gmail.com>
2026-02-08 01:24:58 +0530

    completed results page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import HowItWorks from "./pages/HowItWorks";
 import AboutUs from "./pages/AboutUs";
 import PreInterview from "./pages/PreInterview";
 import Interview from "./pages/Interview";
+import Results from "./pages/Results";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/pre-interview" element={<PreInterview />} />
       <Route path="/interview" element={<Interview />} />
+      <Route path="/results" element={<Results />} />
       <Route path="/how-it-works" element={<HowItWorks />} />
       <Route path="/about-us" element={<AboutUs />} />
     </Routes>
